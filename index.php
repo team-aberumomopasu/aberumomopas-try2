0018fda37d28a6d245df855c0dbc642de5b13fde
Huzitatuguharu <sgrko.4751171@gmail.com>
2021-07-05 20:37:40 +0900

    ブートストラップ

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 include("./config/link.php");
 ?>
-
 <!DOCTYPE html>
 <html lang="ja">
 
 <head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -30,6 +36,10 @@ include("./config/link.php");
 
   <a href="<?= link_login ?>">管理者ログイン</a>
   <!-- 管理者権限を持った人しか使わないボタン -->
+
+
+  <!-- Option 1: Bootstrap Bundle with Popper -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
 </body>
 
