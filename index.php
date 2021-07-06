c569313bee484a3d91a98d78eaad41436ca9270e
Huzitatuguharu <sgrko.4751171@gmail.com>
2021-07-06 10:47:25 +0900

    テストです

--- index.php
+++ index.php
@@ -5,43 +5,93 @@ include("./config/link.php");
 <html lang="ja">
 
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ANIMAL POLICE</title>
-  <link rel="stylesheet" href="#">
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <title>Business Casual - Start Bootstrap Theme</title>
+  <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+  <!-- Font Awesome icons (free version)-->
+  <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+  <!-- Google fonts-->
+  <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet" />
+  <!-- Core theme CSS (includes Bootstrap)-->
+  <link href="css/styles.css" rel="stylesheet" />
 </head>
 
 <body>
-  <a href="<?= link_about ?>">はじめに</a>
-  <a href="<?= link_activity ?>">活動報告</a>
-  <a href="<?= link_signature ?>">ネット署名</a>
-  <a href="<?= link_transfer ?>">譲渡会</a>
-  <a href="<?= link_donate ?>">寄付•グッズ</a>
-  <a href="<?= link_contact ?>">コンタクト</a>
-  <a href="<?= link_SOS ?>">S0S</a>
+  <header>
+    <h1 class="site-heading text-center text-faded d-none d-lg-block">
+      <!-- <span class="site-heading-upper text-primary mb-3">A Free Bootstrap Business Theme</span> -->
+      <span class="site-heading-lower">ANIMAL POLICE FUKUOKA</span>
+    </h1>
+  </header>
+  <!-- Navigation-->
+  <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand text-uppercase fw-bold d-lg-none" href="index.html">ANIMAL POLICE FUKUOKA</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="index.php">ホーム</a></li>
+          <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="about.html">わたしたち</a></li>
+          <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="products.html">活動報告</a></li>
+          <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="store.html">寄付•グッズ</a></li>
+          <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="store.html">SOS</a></li>
+          <li class="nav-item px-lg-4"><a class="nav-link text-uppercase" href="store.html">お問合わせ</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 
-  <h1>ANIMAL</h1>
-  <h1>POLICE</h1>
-  <h1>FUKUOKA</h1>
+  <section class="page-section clearfix">
+    <div class="container">
+      <div class="intro">
+        <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="assets/img/intro.jpg" alt="..." />
+        <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+          <h2 class="section-heading mb-4">
+            <span class="section-heading-upper">Aboutus</span>
+            <span class="section-heading-lower">わたしたち</span>
+          </h2>
+          <p class="mb-4">動物虐待のない世界を目指しています</p>
+          <div class="intro-button mx-auto"><a class="btn btn-dark btn-lg" href="#!">くわしくみる</a></div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="page-section cta">
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <div class="cta-inner bg-faded text-center rounded">
+            <h2 class="section-heading mb-4">
+              <span class="section-heading-upper">Our Promise</span>
+              <span class="section-heading-lower">To You</span>
+            </h2>
+            <p class="mb-0">When you walk into our shop to start your day, we are dedicated to providing you with friendly service, a welcoming atmosphere, and above all else, excellent products made with the highest quality ingredients. If you are not satisfied, please let us know and we will do whatever we can to make things right!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <footer class="footer text-faded text-center py-5">
+    <div class="container">
+      <p class="m-0 small">Copyright &copy; Your Website 2021</p>
+    </div>
+    <!-- <footer class="mt-auto text-white-50"> -->
+    <p>©ANIMAL POLICE FUKUOKA All rights reserved.</p>
 
-  <p>©ANIMAL POLICE FUKUOKA All rights reserved.</p>
+    <a href="<?= link_login ?>">管理者ログイン</a>
+    <!-- 管理者権限を持った人しか使わないボタン -->
 
-  <a href="<?= link_login ?>">管理者ログイン</a>
-  <!-- 管理者権限を持った人しか使わないボタン -->
+    <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+  </footer>
 
-
-  <!-- Option 1: Bootstrap Bundle with Popper -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-
+  <!-- Bootstrap core JS-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- Core theme JS-->
+  <script src="js/scripts.js"></script>
 </body>
-
 
 </html>
